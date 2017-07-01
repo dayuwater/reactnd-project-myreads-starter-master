51f878ee97a1effa9f6c1822542707d796ba7e38
Tan Wang <timmy940408@me.com>
2017-07-01 12:49:00 -0400

    feat: The search page can now search the API and return correct result if the book data is well formatted

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,20 +1,39 @@
 import React , {Component} from 'react';
 import {Link} from 'react-router-dom';
 import Book from './Book';
+import * as BooksAPI from '../BooksAPI'
 
 
 class SearchPage extends Component {
     state ={
-        query : ''
+        query : '',
+        books: []
     }
 
     updateQuery = (query) => {
         this.setState({ query: query.trim() })
+        if(query != ''){
+            BooksAPI.search(query, 30).then((books)=>{
+                if(books.error != "empty query")
+                    this.setState({books})
+                else
+                    this.setState({books:[]})
+                
+            })
+            if(this.state.books["error"] == "empty query"){
+                this.setState({books:[]})
+            }
+        }
+
+        else
+            this.setState({books:[]})
     }
 
     clearQuery = () => {
-        this.setState({query: ''})
+        this.setState({query: '', books:[]})
+       
     }
+
 
     render() {
         return (
@@ -22,37 +41,24 @@ class SearchPage extends Component {
             <div className="search-books-bar">
                 <Link to="/" className="close-search">Close</Link>
                 <div className="search-books-input-wrapper">
-                    <input type="text" placeholder="Search by title or author" 
+                    <input type="text" value={this.state.query} placeholder="Search by title or author" 
                     onChange={(event) => this.updateQuery(event.target.value)} />
                 </div>
-                <button className="clear-search"> Clear </button>
+                <button onClick={() => this.clearQuery()} className="clear-search"> Clear </button>
             </div>
+           
             <div className="search-books-results">
+                 
                 <ol className="books-grid">
-                    <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                    <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                     <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                    <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                    <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                    <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                     <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
-                    <li>
-                        <Book title="Test" author="Test" imgUrl="Test" />
-                    </li>
+                    
+                    {
+                        this.state.books.map((book) => (
+                        <li key={book.id}>
+                            <Book title={book.title} author="" imgUrl={book.imageLinks['thumbnail']} />
+                        </li>
+                    ))}
+                    
+                
                 </ol>
             </div>
           </div>
